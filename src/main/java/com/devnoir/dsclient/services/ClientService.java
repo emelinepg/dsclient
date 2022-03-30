7a8dc5167c18ff12688117a9333edb9e763844e0
emelinepg <emelinedev84@gmail.com>
2022-03-30 01:15:28 -0300

    Find by id, exception handling

--- src/main/java/com/devnoir/dsclient/services/ClientService.java
+++ src/main/java/com/devnoir/dsclient/services/ClientService.java
@@ -1,6 +1,7 @@
 package com.devnoir.dsclient.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devnoir.dsclient.dto.ClientDTO;
 import com.devnoir.dsclient.entities.Client;
 import com.devnoir.dsclient.repositories.ClientRepository;
+import com.devnoir.dsclient.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -21,5 +23,12 @@ public class ClientService {
 	public List<ClientDTO> findAll() {
 		List<Client> list = repository.findAll();
 		return list.stream().map(client -> new ClientDTO(client)).collect(Collectors.toList());
+	}
+	
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> opt = repository.findById(id);
+		Client client = opt.orElseThrow(() -> new ResourceNotFoundException("Client not found"));
+		return new ClientDTO(client);
 	}
 }
