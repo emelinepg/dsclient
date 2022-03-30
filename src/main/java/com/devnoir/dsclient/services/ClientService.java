d82570e85caf666a6aa93df724edc360207f600c
emelinepg <emelinedev84@gmail.com>
2022-03-30 09:02:28 -0300

    Insert, update, delete

--- src/main/java/com/devnoir/dsclient/services/ClientService.java
+++ src/main/java/com/devnoir/dsclient/services/ClientService.java
@@ -4,7 +4,10 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,7 +31,43 @@ public class ClientService {
 	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		Optional<Client> opt = repository.findById(id);
-		Client client = opt.orElseThrow(() -> new ResourceNotFoundException("Client not found"));
+		Client client = opt.orElseThrow(() -> new ResourceNotFoundException("Client id not found: " + id + ". Enter a valid id"));
 		return new ClientDTO(client);
+	}
+	
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client client = new Client();
+		copyDtoToEntity(dto, client);
+		repository.save(client);
+		return new ClientDTO(client);
+	}
+	
+	@Transactional
+	public ClientDTO update(ClientDTO dto, Long id) {
+		try {
+			Client client = repository.getById(id);
+			copyDtoToEntity(dto, client);
+			repository.save(client);
+			return new ClientDTO(client);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Client id not found: " + id + ". Enter a valid id");
+		}
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Client id not found: " + id + ". Enter a valid id");
+		} 
+	}
+
+	private void copyDtoToEntity(ClientDTO dto, Client client) {
+		client.setName(dto.getName());
+		client.setCpf(dto.getCpf());
+		client.setIncome(dto.getIncome());
+		client.setBirthDate(dto.getBirthDate());
+		client.setChildren(dto.getChildren());
 	}
 }
